9ff729dcbd13309ff10ea711d6c605eb94878742
D-Pavlenko <denispavlenko94@gmail.com>
2022-07-24 23:18:14 +0300

    Updates

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+
+$recepient = "agragregra@ya.com";
+$sitename = "FOOTBALL.";
+
+$name = trim($_POST["name"]);
+$email = trim($_POST["email"]);
+$text = trim($_POST["text"]);
+$message = "Name: $name \nEmail: $email \nMessage : $text";
+
+$pagetitle = "NEW ORDER \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
